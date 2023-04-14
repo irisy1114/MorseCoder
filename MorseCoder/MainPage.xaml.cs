432d32c2d83fc5728839fab1b17822d1c39d85ff
Iris Yao <91230673+irisy1114@users.noreply.github.com>
2023-04-13 21:00:04 -0500

    Add project files.

---
+++ MorseCoder/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCoder
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        string code = "";
+        string message = "";
+        void Dot_Clicked(object sender, System.EventArgs e)
+        {
+            code += ".";
+            output_code.Text = code;
+        }
+        void Dash_Clicked(object sender, System.EventArgs e)
+        {
+            code += "-";
+            output_code.Text = code;
+        }
+        void Space_Clicked(object sender, System.EventArgs e)
+        {
+            message += Morse.MorseCoder(code);
+            output.Text = message;
+            code = "";
+            output_code.Text = code;
+        }
+    }
+}
